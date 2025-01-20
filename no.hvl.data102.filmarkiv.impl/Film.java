8a9a7f75e2ef3012031b64d603e16b6348563bed
184887 <184887@stud.hvl.no>
2025-01-20 14:51:59 +0100

    {commit_message}

---
+++ no.hvl.data102.filmarkiv.impl/Film.java
@@ -0,0 +1,119 @@
+public class Film {
+    private int filmnr;
+    private String produsent;
+    private String tittel;
+    private int lanseringsAar;
+    private Sjanger sjanger;
+    private String filmskap;
+
+
+    public enum Sjanger {
+        ACTION, DRAMA, HISTORY, HORROR, MUSICAL, SCIFI, WESTERN
+    }
+
+    public Film(int filmnr, String produsent, String tittel, int lanseringsAar, Sjanger sjanger, String filmskap) {
+        this.filmnr = filmnr;
+        this.produsent = produsent;
+        this.tittel = tittel;
+        this.lanseringsAar = lanseringsAar;
+        this.sjanger = sjanger;
+        this.filmskap = filmskap;
+    }
+
+    public int getFilmnr() {
+        return filmnr;
+    }
+
+    public void setFilmnr(int filmnr) {
+        this.filmnr = filmnr;
+    }
+
+    public String getProdusent() {
+        return produsent;
+    }
+
+    public void setProdusent(String produsent) {
+        this.produsent = produsent;
+    }
+
+    public String getTittel() {
+        return tittel;
+    }
+
+    public void setTittel(String tittel) {
+        this.tittel = tittel;
+    }
+
+    public int getLanseringsAar() {
+        return lanseringsAar;
+    }
+
+    public void setLanseringsAar(int lanseringsAar) {
+        this.lanseringsAar = lanseringsAar;
+    }
+
+    public Sjanger getSjanger() {
+        return sjanger;
+    }
+
+    public void setSjanger(Sjanger sjanger) {
+        this.sjanger = sjanger;
+    }
+
+    public String getFilmskap() {
+        return filmskap;
+    }
+
+    public void setFilmskap(String filmskap) {
+        this.filmskap = filmskap;
+    }
+
+    
+
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + filmnr;
+        result = prime * result + ((produsent == null) ? 0 : produsent.hashCode());
+        result = prime * result + ((tittel == null) ? 0 : tittel.hashCode());
+        result = prime * result + lanseringsAar;
+        result = prime * result + ((sjanger == null) ? 0 : sjanger.hashCode());
+        result = prime * result + ((filmskap == null) ? 0 : filmskap.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Film other = (Film) obj;
+        if (filmnr != other.filmnr)
+            return false;
+        if (produsent == null) {
+            if (other.produsent != null)
+                return false;
+        } else if (!produsent.equals(other.produsent))
+            return false;
+        if (tittel == null) {
+            if (other.tittel != null)
+                return false;
+        } else if (!tittel.equals(other.tittel))
+            return false;
+        if (lanseringsAar != other.lanseringsAar)
+            return false;
+        if (sjanger != other.sjanger)
+            return false;
+        if (filmskap == null) {
+            if (other.filmskap != null)
+                return false;
+        } else if (!filmskap.equals(other.filmskap))
+            return false;
+        return true;
+    }
+}
---
+++ no.hvl.data102.filmarkiv.impl/Filmarkiv.java
@@ -0,0 +1,99 @@
+//Todo 1: Implementere trimme metoder for å fjerne whitespace fra input
+
+public class Filmarkiv implements FilmarkivADT {
+
+    private Film[] filmer;
+    private int antall;
+
+    public Filmarkiv() {
+        filmer = new Film[100];
+        antall = 0;
+    }
+
+    @Override
+    public Film finnFilm(int nr) {
+        for(Film film : filmer) {
+            if(film.getFilmnr() == nr) {
+                return film;
+            } 
+        }
+        return null;
+    }
+
+    @Override
+    public void leggTilFilm(Film nyFilm) {
+        
+        if (antall < filmer.length && soekTittel(nyFilm.getTittel()) == null) {
+            filmer[antall] = nyFilm;
+            antall++;
+        } else{
+            filmer = utvid();
+            filmer[antall] = nyFilm;
+            antall++;
+        }
+        
+    }
+
+    @Override
+    public boolean slettFilm(int filmnr) {
+        int i = 0; 
+        for(Film film : filmer) {
+            if(film.getFilmnr() == filmnr) {
+                filmer[i] = null;
+                return true;
+            }
+            i++;
+        }
+
+        return false; 
+    }
+
+    @Override
+    public Film[] soekTittel(String delstreng) {
+        for(Film film : filmer) {
+            if(film.getTittel().contains(delstreng)) {
+                return new Film[]{film};
+            }
+        }
+
+        return null;
+
+    }
+
+    @Override
+    public Film[] soekProdusent(String delstreng) {
+        for(Film film : filmer) {
+            if(film.getFilmskap().contains(delstreng)) {
+                return new Film[]{film};
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public int antall(Sjanger sjanger) {
+        int i = 0;
+        for(Film film : filmer) {
+            if(film.getSjanger().equals(sjanger)) {
+                i++;
+            }
+        }
+        return i;
+    }
+
+    @Override
+    public int antall() {
+        return antall;
+    }
+
+    public Film[] utvid(){
+        Film[] nyFilmer = new Film[filmer.length * 2];
+        for(int i = 0; i < filmer.length; i++) {
+            nyFilmer[i] = filmer[i];
+        }
+        return nyFilmer;
+    }
+    }
+
+    
